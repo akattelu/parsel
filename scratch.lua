c59814bfcbecb68a69ba9880cb090a8928093381
Aakash Kattelu <aakash.kattelu@gmail.com>
2024-09-30 14:09:41 -0700

    Rewrite scratch file

--- scratch.lua
+++ scratch.lua
@@ -6,44 +6,52 @@ if file then
   contents = file:read("*a")
 end
 
-local Parsers = {}
 
-local localKeywordParser = p.map(p.literal("local"), function(_) return { type = "keyword", value = "local" } end)
-local equalParser = p.map(p.literal("="), function(_) return { type = "keyword", value = "=" } end)
-local identParser = p.map(p.seq(p.letter(), p.zeroOrMore(p.either(p.letter(), p.digit()))), function(tokens)
-  return {
-    type = "identifier",
-    value = tokens[1] .. table.concat(tokens[2], "")
-  }
-end)
-local intParser = p.map(p.oneOrMore(p.digit()),
-  function(digList) return { type = "number", value = tonumber(table.concat(digList, "")) } end)
-local floatParser = p.map(p.seq(intParser, p.literal("."), intParser),
-  function(digList) return { type = "number", value = tonumber(table.concat(digList, "")) } end)
-local numberParser = p.either(intParser, floatParser)
+--- Return a function that selects the nth item from a sequence
+local pick = function(n)
+  return function(seq)
+    return seq[n]
+  end
+end
 
-local stringParser = p.map(p.seq(p.literal('"'), p.zeroOrMore(p.literalBesides('"')), p.literal('"')),
+local Parsers = {
+}
+
+-- Keywords
+Parsers.localDecl = p.map(p.literal("local"), function(_) return { type = "keyword", value = "local" } end)
+Parsers.equals = p.map(p.literal("="), function(_) return { type = "EQUALS", value = "=" } end)
+
+-- Primitives
+Parsers.ident = p.map(p.seq(p.letter(), p.zeroOrMore(p.either(p.letter(), p.digit()))),
+  function(tokens) return { type = "identifier", value = tokens[1] .. table.concat(tokens[2], "") } end)
+Parsers.int = p.map(p.oneOrMore(p.digit()),
+  function(digList) return { type = "number", value = tonumber(table.concat(digList, "")) } end)
+
+Parsers.string = p.map(p.seq(p.literal('"'), p.zeroOrMore(p.literalBesides('"')), p.literal('"')),
   function(seq)
     return {
       type = "string",
       value = table.concat(seq[2], "")
     }
   end)
+Parsers.float = p.map(p.seq(Parsers.int, p.literal("."), Parsers.int),
+  function(digList) return { type = "number", value = tonumber(table.concat(digList, "")) } end)
+Parsers.number = p.either(Parsers.int, Parsers.float)
 
+-- Expressions
+Parsers.primitiveExpression = p.any(Parsers.number, Parsers.string, Parsers.ident)
+Parsers.parenthesizedExpression = p.map(
+  p.seq(p.literal("("), p.lazy(function() return Parsers.expression end),
+    p.literal(")")), pick(2))
+Parsers.baseExpression = p.either(Parsers.primitiveExpression, Parsers.parenthesizedExpression)
+Parsers.expression = p.any(p.lazy(function() return Parsers.infixExpression end), Parsers.baseExpression)
 
-local expressionParser
-local infixExpressionParser
-local primitiveExpressionParser = p.any(numberParser, stringParser, identParser)
-local parenthesizedExpressionParser = p.map(p.seq(p.literal("("), p.lazy(function() return expressionParser end),
-  p.literal(")")), function(seq) return seq[2] end)
-local baseExpressionParser = p.either(primitiveExpressionParser, parenthesizedExpressionParser)
-expressionParser = p.any(p.lazy(function() return infixExpressionParser end), baseExpressionParser)
-infixExpressionParser = p.map(
-  p.seq(baseExpressionParser,
+Parsers.infixExpression = p.map(
+  p.seq(Parsers.baseExpression,
     p.optionalWhitespace(),
-    p.any(p.literal("+"), p.literal("-"), p.literal("*"), p.literal("/"), p.literal("^")),
+    p.anyLiteral("+", "-", "^", "*", "==", "~=", "^"),
     p.optionalWhitespace(),
-    expressionParser
+    Parsers.expression
   ), function(seq)
     return {
       type = "infix_expression",
@@ -53,34 +61,39 @@ infixExpressionParser = p.map(
     }
   end
 )
-local expressionStmtParser = expressionParser
-local assignmentStmtParser = p.map(
-  p.seq(
-    p.optional(localKeywordParser)
-    , p.optionalWhitespace()
-    , identParser
-    , p.optionalWhitespace()
-    , equalParser
-    , p.optionalWhitespace()
-    , expressionParser
-  ), function(results)
-    local scope
-    if results[1] == p.nullResult then
-      scope = "GLOBAL"
-    else
-      scope = "LOCAL"
-    end
-    return {
-      type = "assignment",
-      ident = results[3],
-      value = results[7],
-      scope = scope,
-    }
-  end)
-local stmtParser = p.any(assignmentStmtParser, expressionStmtParser)
-local programParser = p.oneOrMore(p.map(p.seq(stmtParser, p.newline()), function(x) return x[1] end))
+-- Statements
+Parsers.expressionStatement = Parsers.expression
+Parsers.assignment =
+    p.map(
+      p.seq(
+        p.optional(Parsers.localDecl)
+        , p.optionalWhitespace()
+        , Parsers.ident
+        , p.optionalWhitespace()
+        , Parsers.equals
+        , p.optionalWhitespace()
+        , Parsers.expression
+      ), function(results)
+        local scope
+        if results[1] == p.nullResult then
+          scope = "GLOBAL"
+        else
+          scope = "LOCAL"
+        end
+        return {
+          type = "assignment",
+          ident = results[3],
+          value = results[7],
+          scope = scope,
+        }
+      end)
 
-local parsed = p.parse(contents, programParser)
+Parsers.statement = p.any(Parsers.assignment, Parsers.expressionStatement)
+
+-- Program
+Parsers.program = p.oneOrMore(p.map(p.seq(Parsers.statement, p.newline()), function(x) return x[1] end))
+
+local parsed = p.parse(contents, Parsers.program)
 if not parsed.parser:succeeded() then
   print(parsed.parser.error)
   os.exit(1)
